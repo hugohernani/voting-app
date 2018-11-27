e68bf7dc9aca9b9693566f854f2dab485b0b957e
Hugo Hernani <hugo@quezmedia.com>
2018-11-26 21:17:36 -0300

    Configure SMTP for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,13 +93,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USER'] || '',
-    password:             ENV['GMAIL_PASSWORD'] || '',
-    authentication:       :plain,
-    enable_starttls_auto: true
+    :domain               => ENV['SMTP_DOMAIN'],
+    :perform_deliveries   => true,
+    :address              => ENV['SMTP_ADDRESS'],
+    :port                 => ENV['SMTP_PORT'],
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => :login,
+    :enable_starttls_auto => true
   }
 
   config.action_mailer.default_url_options = {
